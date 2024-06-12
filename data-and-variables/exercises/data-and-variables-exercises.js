a25a1cf440512e10e58e9ccf3c91c6cb7d7b3a73
GrizzlyMain <ryan.thom.dalton@gmail.com>
2024-06-12 14:17:27 -0400

    Commit message

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -9,3 +9,20 @@
 // Calculate a trip to the moon below
 
 // Print the results of the trip to the moon below
+
+let nameOfSpaceShuttle = "GritandGrind1"
+let shuttleSpeed = 17500
+let distanceToMars = 225000000
+let distanceToMoon = 384400
+const milesPerKm = .621
+
+console.log (nameOfSpaceShuttle);
+console.log(shuttleSpeed);
+console.log (distanceToMars);
+console.log (distanceToMoon);
+console.log (milesPerKm);
+
+let milesToMars = distanceToMars * milesPerKm;
+let hoursToMars = milesToMars / shuttleSpeed;
+let daysToMars = hoursToMars / 24;
+console.log (nameOfSpaceShuttle + " will take " + daysToMars + " days to reach Mars.");
