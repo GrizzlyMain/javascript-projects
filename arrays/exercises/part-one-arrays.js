53decb6fb32b49fc981e35eb8c7fc3f75f97f270
GrizzlyMain <ryan.thom.dalton@gmail.com>
2024-06-19 14:39:03 -0500

    Commit message

--- arrays/exercises/part-one-arrays.js
+++ arrays/exercises/part-one-arrays.js
@@ -3,3 +3,9 @@
 //Use the bracket notation method to add "42" and "hello" to the array. Add these new items one at a time.  Print the array after each step to confirm the changes.
 
 //Use a single .push() to add the following items: false, -4.6, and "87".  Print the array to confirm the changes.
+let practiceFile = [273.15];
+
+practiceFile[1] = 42;
+practiceFile.push('hello');
+practiceFile.push(false, -4.6, 87);
+console.log(practiceFile);
