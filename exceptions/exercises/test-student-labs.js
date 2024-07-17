c635d84bf9a0d315c3b112c0e92ebadd67452a87
GrizzlyMain <ryan.thom.dalton@gmail.com>
2024-07-17 14:11:46 -0500

    Commit message

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,10 +1,21 @@
 function gradeLabs(labs) {
+  try {
   for (let i=0; i < labs.length; i++) {
-    let lab = labs[i];
-    let result = lab.runLab(3);
-    console.log(`${lab.student} code worked: ${result === 27}`);
+        let lab = labs[i];
+        let result = lab.runLab(3);
+        console.log(`${lab.student} code worked: ${result === 27}`);''
+      }
+  if (!lab.runLab()){
+    throw new SyntaxError('runLab is not defined.')
   }
 }
+catch(err) {
+  console.log('Error thrown.');
+}
+
+}
+  
+
 
 let studentLabs = [
   {
@@ -20,5 +31,27 @@ let studentLabs = [
     }
   }
 ];
+
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
 
 gradeLabs(studentLabs);
